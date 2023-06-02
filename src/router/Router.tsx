60f11ceeac02046cc4a223051803fc5b124ad2ca
eunyeong <123078739+jjangeunyeong@users.noreply.github.com>
2023-06-02 18:23:07 +0900

    Feat/layout create basic layout(header,footer) (#1) (#2) (#6)
    * design : 헤더,푸터 생성
    
    * fix : 폴더구조 수정
    
    Co-authored-by: J9 <93317914+jgkang9402@users.noreply.github.com>

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,14 +1,14 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import MainPage from '@components/Pages/Main/Main';
 import AccompanyPage from '@components/Pages/Accompany/Accompany';
 import React from 'react';
 
 const Router = () => {
   return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/accompany" element={<AccompanyPage />} />
-    </Routes>
+    <Switch>
+      <Route path="/" component={MainPage} />
+      <Route path="/accompany" component={AccompanyPage} />
+    </Switch>
   );
 };
 
