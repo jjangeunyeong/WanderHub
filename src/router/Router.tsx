8a1cc4e9367fe18f655f07313d5d373676e8c5d0
Eunyeong Jang <jangeunyeong081@gmail.com>
2023-06-04 17:08:23 +0900

    fix : react-router-dom v6 인스톨로 오류 해결

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,14 +1,14 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import MainPage from '@components/Pages/Main/Main';
 import AccompanyPage from '@components/Pages/Accompany/Accompany';
 import React from 'react';
 
 const Router = () => {
   return (
-    <Switch>
-      <Route path="/" component={MainPage} />
-      <Route path="/accompany" component={AccompanyPage} />
-    </Switch>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/accompany" element={<AccompanyPage />} />
+    </Routes>
   );
 };
 
